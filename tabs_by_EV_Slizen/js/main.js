6a73fe2f8b727238ac6d1a1f0d7ff2fa4d472450
Evgeniy <jdslonim@gmail.com>
2019-01-08 23:32:55 +0300

    7 commit

--- tabs_by_EV_Slizen/js/main.js
+++ tabs_by_EV_Slizen/js/main.js
@@ -33,4 +33,56 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+
+    //Timer
+
+    let deadLine = '2019-01-08 24:00';
+
+    function getTimeRemaning (endtime) {
+        let t = Date.parse(deadLine) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaning(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+            if (hours.textContent.length <= 1) {
+                hours.textContent = "0" + t.hours;
+            }
+            if (minutes.textContent.length <= 1) {
+                minutes.textContent = "0" + t.minutes;
+            }
+            if (seconds.textContent.length <= 1) {
+                seconds.textContent = "0" + t.seconds;
+            }
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+            }
+        }
+    }
+
+    setClock('timer', deadLine);
+
 });
